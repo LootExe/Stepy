f0dd56060418272a6d66f2e7eecb4d23439fb0b1
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-10 12:03:48 +1100

    removed licenses from settings page

--- lib/src/screen/settings_screen.dart
+++ lib/src/screen/settings_screen.dart
@@ -1,39 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 
 import '../bloc/settings_bloc.dart';
-import '../string_extension.dart';
 import 'widget/settings_tile.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  var _packageInfo = PackageInfo(
-    appName: 'Unknown',
-    packageName: 'Unknown',
-    version: 'Unknown',
-    buildNumber: 'Unknown',
-    buildSignature: 'Unknown',
-  );
-
-  Future<void> _initPackageInfo() async {
-    final info = await PackageInfo.fromPlatform();
-    setState(() {
-      _packageInfo = info;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _initPackageInfo();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +18,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
             itemExtent: 70.0,
             children: [
               SettingsTile(
-                title: 'Licenses',
-                subtitle: 'Show licenses and app details',
-                onTap: () => showLicensePage(
-                  context: context,
-                  applicationName: '${_packageInfo.appName.capitalize()} App',
-                  applicationVersion: 'v${_packageInfo.version}',
-                  applicationIcon: const Image(
-                    image: AssetImage('asset/icon/launcher_icon.png'),
-                    width: 64.0,
-                    height: 64.0,
-                  ),
-                ),
+                title: 'Settings Tile',
+                subtitle: 'Click to change something',
+                onTap: () {},
               ),
             ],
           ),
