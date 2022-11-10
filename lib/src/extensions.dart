aad8a92a14d6fe982b9441f8338b9c831ebe99f8
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-10 12:05:38 +1100

    date time and string extensions

---
+++ lib/src/extensions.dart
@@ -0,0 +1,25 @@
+extension DateUtils on DateTime {
+  bool get isToday {
+    final now = DateTime.now();
+    return now.day == day && now.month == month && now.year == year;
+  }
+
+  bool get isYesterday {
+    final yesterday = DateTime.now().subtract(const Duration(days: 1));
+    return yesterday.day == day &&
+        yesterday.month == month &&
+        yesterday.year == year;
+  }
+
+  bool get isPastDay {
+    final now = DateTime.now();
+    final today = DateTime(now.year, now.month, now.day);
+    return isBefore(today);
+  }
+}
+
+extension CapExtension on String {
+  String capitalize() {
+    return (length > 1) ? this[0].toUpperCase() + substring(1) : toUpperCase();
+  }
+}
