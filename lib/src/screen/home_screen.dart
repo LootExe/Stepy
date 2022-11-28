0d71e6c031b266d0e482dbf189a17abbf05b3ccc
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-28 12:01:22 +1100

    if the app gaines focus, pedometer sensor is started and steps are streamed. On focus loss, sensor readings are stopped.

--- lib/src/screen/home_screen.dart
+++ lib/src/screen/home_screen.dart
@@ -1,47 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'widget/date_bar.dart';
-import 'widget/footer_bar.dart';
-import 'widget/header_bar.dart';
-import 'widget/menu_drawer.dart';
-import 'widget/steps_indicator.dart';
+import '../bloc/pedometer_bloc.dart';
+import '../bloc/settings_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
-  HomeScreen({Key? key}) : super(key: key);
+import 'widget/home_menu.dart';
+import 'widget/step_progress_bar.dart';
+import 'widget/timeline.dart';
 
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
+  late PedometerBloc _pedometerBloc;
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        _pedometerBloc.add(PedometerStarted());
+        break;
+      case AppLifecycleState.paused:
+        _pedometerBloc.add(PedometerStopped());
+        break;
+      default:
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _pedometerBloc = BlocProvider.of<PedometerBloc>(context);
+    _pedometerBloc.add(PedometerStarted());
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      drawer: MenuDrawer(),
       body: Container(
-        margin: const EdgeInsets.symmetric(
-          vertical: 64.0,
-          horizontal: 16.0,
-        ),
-        child: Column(
-          children: [
-            HeaderBar(
-              onButtonPressed: () => _scaffoldKey.currentState?.openDrawer(),
-              date: DateTime.now(),
-              dateRange: 'Daily Activity',
-            ),
-            Spacer(flex: 2),
-            DateBar(),
-            Spacer(),
-            StepsIndicator(
-              currentSteps: 7500,
-              stepsGoal: 10000,
-            ),
-            Spacer(),
-            FooterBar(
-              distance: 5.6,
-              calories: 1589,
-              avgSteps: 8762,
-            ),
-          ],
+        margin: const EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
+        child: BlocBuilder<PedometerBloc, PedometerState>(
+          builder: (context, state) => Stack(
+            children: [
+              Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: HomeMenu(),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(20.0),
+                    child: StepProgressBar(
+                      currentSteps: state.stepsDaily,
+                      stepsGoal: BlocProvider.of<SettingsBloc>(context)
+                          .settings
+                          .dailyStepGoal,
+                      stepsRecord: state.stepsRecord,
+                      averageSteps: state.stepsAverage,
+                      progressColor: Theme.of(context).colorScheme.secondary,
+                      backgroundColor: Colors.grey,
+                      stepFontSize: 64.0,
+                      strokeWidth: 1.5,
+                    ),
+                  ),
+                ],
+              ),
+              Positioned.fill(
+                top: 330.0,
+                child: Timeline(stepHistory: state.stepsHistory),
+              ),
+            ],
+          ),
         ),
       ),
     );
