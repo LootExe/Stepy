f6394ef3dd5fd5186569c9a17deffe9e10c9cd60
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-28 11:55:21 +1100

    on app start circular bar is shown, data loading is happening before actual app start

--- lib/app.dart
+++ lib/app.dart
@@ -1,67 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'src/model/app_initialize_data.dart';
+
+import 'src/bloc/background_bloc.dart';
 import 'src/bloc/settings_bloc.dart';
+import 'src/bloc/pedometer_bloc.dart';
+
+import 'src/repository/history_repository.dart';
+import 'src/repository/pedometer_repository.dart';
 import 'src/repository/settings_repository.dart';
+
 import 'src/screen/home_screen.dart';
+import 'src/screen/permission_screen.dart';
+
 import 'src/theme_manager.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  const App({super.key, required this.initData});
 
-  Future<bool> _loadData(BuildContext context) async {
-    await Future.wait([
-      RepositoryProvider.of<SettingsRepository>(context).readSettings(),
-      //RepositoryProvider.of<NotesRepository>(context).readNotes(),
-      SystemChrome.setPreferredOrientations(
-        [
-          DeviceOrientation.portraitUp,
-          DeviceOrientation.portraitDown,
-        ],
-      ),
-    ]);
-
-    return true;
-  }
+  final AppInitializeData initData;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _loadData(context),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return MultiBlocProvider(
-            providers: [
-              BlocProvider(
-                create: (context) => SettingsBloc(
-                    repository:
-                        RepositoryProvider.of<SettingsRepository>(context)),
-              ),
-              /* BlocProvider(
-                create: (context) => NoteListBloc(
-                    RepositoryProvider.of<NotesRepository>(context)),
-              ), */
-            ],
-            child: BlocBuilder<SettingsBloc, SettingsState>(
-              builder: (context, state) => MaterialApp(
-                theme: ThemeManager.lightTheme,
-                darkTheme: ThemeManager.darkTheme,
-                themeMode: ThemeMode.dark,
-                home: HomeScreen(),
-              ),
-            ),
-          );
-        } else {
-          return const Center(
-            child: SizedBox(
-              width: 60,
-              height: 60,
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              SettingsBloc(RepositoryProvider.of<SettingsRepository>(context)),
+        ),
+        BlocProvider(
+          create: (context) => PedometerBloc(
+            pedometer: RepositoryProvider.of<PedometerRepository>(context),
+            history: RepositoryProvider.of<HistoryRepository>(context),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => BackgroundBloc(
+            isRunning: initData.isForegroundRunning,
+          ),
+        ),
+      ],
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) => MaterialApp(
+          theme: ThemeManager.lightTheme,
+          darkTheme: ThemeManager.darkTheme,
+          themeMode: ThemeMode.dark,
+          home: initData.isPedometerPermissionGranted
+              ? const HomeScreen()
+              : const PermissionScreen(),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,32 +3,93 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
-//import 'src/repository/notes_repository.dart';
+
+import 'src/bloc/background_bloc.dart';
+import 'src/bloc/pedometer_bloc.dart';
+
+import 'src/model/app_initialize_data.dart';
+import 'src/model/log_data.dart';
+import 'src/model/step_data.dart';
+
+import 'src/provider/hive_database.dart';
+import 'src/provider/pedometer_sensor.dart';
+import 'src/provider/preferences_storage.dart';
+
+import 'src/repository/history_repository.dart';
+import 'src/repository/pedometer_repository.dart';
 import 'src/repository/settings_repository.dart';
 
-void main() {
-  // TODO: Activate later
-  //GoogleFonts.config.allowRuntimeFetching = false;
+void main() async {
+  GoogleFonts.config.allowRuntimeFetching = false;
 
+  runApp(const Center(
+    child: SizedBox(
+      width: 64,
+      height: 64,
+      child: CircularProgressIndicator(
+        color: Colors.greenAccent,
+      ),
+    ),
+  ));
+
+  // Add additional licenses
   LicenseRegistry.addLicense(() async* {
-    final license =
-        await rootBundle.loadString('asset/google_fonts/LICENSE.txt');
+    final license = await rootBundle.loadString('asset/google_fonts/OFL.txt');
     yield LicenseEntryWithLineBreaks(['google_fonts'], license);
   });
+
+  // Create all data repositories
+  final settings = SettingsRepository(
+    storage: PreferencesStorage(),
+  );
+
+  final pedometer = PedometerRepository(
+    storage: PreferencesStorage(),
+    sensor: PedometerSensor(),
+  );
+
+  final history = HistoryRepository(
+    database: HiveDatabase(boxName: StepData.boxName),
+  );
+
+  // Hive register adapters
+  Hive.registerAdapter(StepDataAdapter());
+  Hive.registerAdapter(LogDataAdapter());
+  await Hive.initFlutter();
+
+  // Await all async operations
+  await Future.wait([
+    // Set system orientation to portrait only
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]),
+
+    // Load repository data
+    settings.readSettings(),
+    pedometer.initialize(),
+    history.initialize(),
+
+    // Initialize alarm manager plugin
+    BackgroundBloc.initializeAlarmManager(),
+  ]);
+
+  final appInitData = AppInitializeData(
+    isForegroundRunning: await BackgroundBloc.isServiceRunning,
+    isPedometerPermissionGranted: await PedometerBloc.isPermissionGranted,
+  );
 
   runApp(
     MultiRepositoryProvider(
       providers: [
-        RepositoryProvider<SettingsRepository>(
-          create: (_) => SettingsRepository(),
-        ),
-        /* RepositoryProvider<NotesRepository>(
-          create: (_) => NotesRepository(),
-        ), */
+        RepositoryProvider<SettingsRepository>(create: (_) => settings),
+        RepositoryProvider<PedometerRepository>(create: (_) => pedometer),
+        RepositoryProvider<HistoryRepository>(create: (_) => history),
       ],
-      child: const App(),
+      child: App(initData: appInitData),
     ),
   );
 }
