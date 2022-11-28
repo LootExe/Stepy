438972f7425d7d809b1aadf834e7ea3ea15ccd2f
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-28 11:52:34 +1100

    updated pedometer bloc

--- lib/src/bloc/pedometer_bloc.dart
+++ lib/src/bloc/pedometer_bloc.dart
@@ -1,53 +1,115 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+import '../extensions.dart';
+import '../model/step_data.dart';
+import '../repository/history_repository.dart';
 import '../repository/pedometer_repository.dart';
 
 part 'pedometer_state.dart';
 part 'pedometer_event.dart';
 
 class PedometerBloc extends Bloc<PedometerEvent, PedometerState> {
-  PedometerBloc({required PedometerRepository repository})
-      : _repository = repository,
-        super(PedometerInitial()) {
+  PedometerBloc({
+    required PedometerRepository pedometer,
+    required HistoryRepository history,
+  })  : _pedometer = pedometer,
+        _history = history,
+        super(PedometerInitial(
+          pedometer.stepsCurrentDay,
+          history.dailyRecord,
+          history.dailyAverage,
+          _reverseHistory(history.dailyHistory),
+        )) {
     on<PedometerStarted>(_onStarted);
     on<PedometerStopped>(_onStopped);
-    on<PedometerNewDataReceived>(_onDataReceived);
     on<PedometerFailed>(_onFailed);
-
-    add(PedometerStarted());
+    on<PedometerNewDataReceived>(_onDataReceived);
   }
 
-  final PedometerRepository _repository;
+  final PedometerRepository _pedometer;
+  final HistoryRepository _history;
+  StreamSubscription<int>? _stream;
 
   /// Start the Pedometer UI Stream
   Future<void> _onStarted(
       PedometerEvent event, Emitter<PedometerState> emit) async {
-    await _repository.listen(
-      onData: (steps) => add(PedometerNewDataReceived(steps)),
+    _stream?.cancel();
+
+    // Add one entry on first app start
+    if (_history.dailyHistory.isEmpty) {
+      await _history.add(StepData());
+    }
+
+    _stream = _pedometer.stepCountStream.listen(
+      (dailySteps) async {
+        final lastEntry = _history.dailyHistory.last;
+
+        if (lastEntry.timestamp.isToday) {
+          lastEntry.steps = dailySteps;
+          await lastEntry.save();
+        } else {
+          await _history.add(StepData()
+            ..steps = dailySteps
+            ..timestamp = DateTime.now());
+        }
+
+        add(PedometerNewDataReceived(dailySteps));
+      },
       onError: (error) => add(PedometerFailed()),
+      cancelOnError: true,
     );
   }
-
-  // TODO: OnAppPause == true : _onStopped;
 
   /// Stop Pedometer UI Stream
   Future<void> _onStopped(
       PedometerEvent event, Emitter<PedometerState> emit) async {
-    await _repository.cancel();
-    await _repository.writeData();
+    _stream?.cancel();
 
-    emit(PedometerStopSuccess());
+    emit(PedometerStopSuccess(
+      _pedometer.stepsCurrentDay,
+      _history.dailyRecord,
+      _history.dailyAverage,
+      _reverseHistory(_history.dailyHistory),
+    ));
   }
 
+  /// New daily step count received
   Future<void> _onDataReceived(
       PedometerNewDataReceived event, Emitter<PedometerState> emit) async {
-    emit(PedometerNewData(event.steps));
+    emit(PedometerNewData(
+      event.steps,
+      _history.dailyRecord,
+      _history.dailyAverage,
+      _reverseHistory(_history.dailyHistory),
+    ));
   }
 
+  /// Any kind of sensor error
   Future<void> _onFailed(
       PedometerEvent event, Emitter<PedometerState> emit) async {
-    emit(const PedometerStartFailure('Sensor error'));
+    emit(const PedometerStartFailure('Sensor error', 0, 0, 0, []));
   }
+
+  static List<StepData> _reverseHistory(List<StepData> entries) {
+    final List<StepData> list = [];
+
+    if (entries.length <= 1) {
+      return [];
+    }
+
+    for (int i = entries.length - 2; i >= 0; i--) {
+      list.add(entries[i]);
+    }
+
+    return list;
+  }
+
+  static Future<bool> get isPermissionGranted async =>
+      await Permission.activityRecognition.isGranted;
+
+  static Future<bool> requestPermission() async =>
+      await Permission.activityRecognition.request().isGranted;
 }
--- lib/src/bloc/pedometer_state.dart
+++ lib/src/bloc/pedometer_state.dart
@@ -1,21 +1,54 @@
 part of 'pedometer_bloc.dart';
 
 abstract class PedometerState {
-  const PedometerState();
+  const PedometerState(
+    this.stepsDaily,
+    this.stepsRecord,
+    this.stepsAverage,
+    this.stepsHistory,
+  );
+
+  final int stepsDaily;
+  final int stepsRecord;
+  final int stepsAverage;
+  final List<StepData> stepsHistory;
 }
 
-class PedometerInitial extends PedometerState {}
+class PedometerInitial extends PedometerState {
+  const PedometerInitial(
+    super.stepsDaily,
+    super.stepsRecord,
+    super.stepsAverage,
+    super.stepsHistory,
+  );
+}
 
 class PedometerStartFailure extends PedometerState {
-  const PedometerStartFailure(this.error);
+  const PedometerStartFailure(
+    this.error,
+    super.stepsDaily,
+    super.stepsRecord,
+    super.stepsAverage,
+    super.stepsHistory,
+  );
 
   final String error;
 }
 
-class PedometerStopSuccess extends PedometerState {}
+class PedometerStopSuccess extends PedometerState {
+  const PedometerStopSuccess(
+    super.stepsDaily,
+    super.stepsRecord,
+    super.stepsAverage,
+    super.stepsHistory,
+  );
+}
 
 class PedometerNewData extends PedometerState {
-  const PedometerNewData(this.steps);
-
-  final int steps;
+  const PedometerNewData(
+    super.stepsDaily,
+    super.stepsRecord,
+    super.stepsAverage,
+    super.stepsHistory,
+  );
 }
