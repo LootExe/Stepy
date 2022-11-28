85c3d52220b70aa301d6fd20d5246caf64096025
LootExe <114713024+LootExe@users.noreply.github.com>
2022-11-28 11:45:48 +1100

    removed app info data and repository

--- lib/src/bloc/app_info_event.dart
+++
@@ -1,5 +0,0 @@
-part of 'app_info_bloc.dart';
-
-abstract class AppInfoEvent {}
-
-class AppInfoLoaded extends AppInfoEvent {}
--- lib/src/model/app_info_data.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-import 'package:package_info_plus/package_info_plus.dart';
-
-class AppInfoData {
-  PackageInfo? packageInfo;
-  AssetImage? appIcon;
-}
